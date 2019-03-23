0bbaaa5ba2ac955da5c933748c94a3b8e4299a9e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-23 05:17:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  i = 0
+  array = (1..num).to_a
+  bool = false
+  while i < array.length
+    if i > 1 and i < num
+      if num % i ==0
+        bool = false
+      else
+        bool = true
+      end
+    end
+  end
+  return bool
+end
