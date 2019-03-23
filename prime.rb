c9dcd2f62c2dc6134229f728e3987d982037b3c6
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-23 05:22:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   i = 0
   array = (1..num).to_a
   while i < array.length
-    if i > 1 and i < num
+    if i > 1 and i =< num
       if num % i ==0
         bool = true
       else
